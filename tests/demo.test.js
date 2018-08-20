dca112b157c635376a93c43849a670f719d37a02
JimmyFonteneau <jimmyfo@yahoo.com>
2018-08-20 15:48:38 +0200

    failing test

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -5,7 +5,7 @@ var User = function(name){
 describe("Checking if the user is created correctly", function(){
  it("should create the user with the correct name", function(){
  debugger
- var tom = new User("tom");
+ var tom = new User("tommy");
  tom.name.should.be.equal("tom");
  });
 });
